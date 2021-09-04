9e85194321c25c31be3fb6c02a023d1441ba7fc5
rhedwan <adesolaridwan2003@gmail.com>
2021-09-04 22:02:22 +0100

    How passing 'arguments' works: value and reference.

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/* 
 const bookings = [] ;
 
 const createBooking = function (flightNum, numPassengers=1, price=199 * numPassengers) {
@@ -24,3 +24,34 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+ */
+
+const flight = 'LH234' ;
+const rhedwan = {
+    name: 'Ridwan Adeyemo',
+    passport: 2344252226344
+};
+
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name ;
+    if (passenger.passport === 2344252226344) {
+        alert('Checked In');
+    }else{
+        alert('Wrong passport')
+    }
+}
+
+// checkIn(flight, rhedwan);
+// console.log(flight);
+// console.log(rhedwan);
+
+// const flightNum = flight ;
+// const passenger = rhedwan;
+
+
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 1000000000) ;
+};
+newPassport(rhedwan);
+checkIn(flight, rhedwan);
