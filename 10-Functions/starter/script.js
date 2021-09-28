06509fc7819f879aff106f1a01cd506d864de550
rhedwan <adesolaridwan2003@gmail.com>
2021-09-28 17:46:04 +0100

    Coding Challenges on 'bind' and 'call' method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -213,4 +213,31 @@ const poll = {
     options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
+    // Get answer
+    registerNewAnswer(){
+        const answer = Number(prompt(`${this.question} \n${this.options.join('\n')}\n (Write option number)`))
+        console.log(answer)
+
+        // Register
+        typeof answer == 'number' && answer < this.answers.length && this.answers[answer]++
+        // console.log(this.answers);
+        this.displayResults();
+        this.displayResults('string');
+    },
+    displayResults(type='array'){
+        if (type === 'array'){
+            console.log(this.answers);
+        }else if(type === 'string'){
+            console.log(`Poll result are ${this.answers.join(', ')}`);
+        }
+    }
 };
+
+// poll.registerNewAnswer()
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+poll.displayResults.call({answers: [5, 2, 3]})
+poll.displayResults.call({answers: [5, 2, 3]}, 'string')
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, 'string')
+
+// Data 1: [5, 2, 3]
+// § Data 2: [1, 5, 3, 9, 6, 1]
