15e9c688035178265b88c21433ebef2960b3d620
rhedwan <adesolaridwan2003@gmail.com>
2021-10-07 18:37:48 +0100

    Looping Arrays: forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,10 +71,10 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
+/* 
 let arr = ['a', 'b', 'c', 'd', 'e'] ;
 // SLICE 
 console.log(arr.slice(2));
@@ -108,4 +108,34 @@ console.log(letters);
 console.log([...arr, ...arr2]);
 
 // JOIN
-console.log(letters.join('-'));
+console.log(letters.join('-')); */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for(const movement of movements){
+for(const [i, movement] of movements.entries()){
+  if(movement >0){
+    console.log(`Movement ${i+1} You deposited ${movement}`);
+  }else{
+    console.log(`Movement ${i+1} You withdraw ${Math.abs(movement)}`);
+  }
+};
+
+console.log('_____FOREACH_____')
+
+movements.forEach(function (movement, index, array) {
+  if(movement >0){
+    console.log(`Movement ${index+1} You deposited ${movement}`);
+  }else{
+    console.log(`Movement ${index+1} You withdraw ${Math.abs(movement)}`);
+  }
+});
+
+console.log('======Using the Arrow function======');
+movements.forEach((movement, index, array)=>{
+  if(movement >0){
+    console.log(`Movement ${index+1} You deposited ${movement}`);
+  }else{
+    console.log(`Movement ${index+1} You withdraw ${Math.abs(movement)}`);
+  }
+});
