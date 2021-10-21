f1ab0c4f9ff835421ab452579b8f71f7722002a3
rhedwan <adesolaridwan2003@gmail.com>
2021-10-21 18:48:39 +0100

    Magic of Chaining Methods on Bankist

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -69,7 +69,7 @@ const displayMovements = function(movements){
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov}€</div>
       </div>`;
     
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -80,10 +80,29 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance =  movements.reduce((acc, mov) => acc + mov) ;
-  labelBalance.textContent = `${balance} EUR` ;
+  labelBalance.textContent = `${balance} €` ;
 
 };
 calcDisplayBalance(account1.movements);
+
+
+const calcDisplaySummary  = function (movements) {
+  const income = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${income}€` ;
+
+  const out  = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0); 
+  labelSumOut.textContent = `${Math.abs(out)}€` ;
+
+  const interest = movements
+  .filter(mov => mov > 0)
+  .map(deposit => deposit *1.2/100)
+  .filter( int => int >= 1)
+  .reduce((acc, int)=> acc + int,0) ;
+  labelSumInterest.textContent = `${interest}€` ;
+
+};
+
+calcDisplaySummary(account1.movements);
 
 const createsUsernames = function (accs) {
   accs.forEach(function(acc){
