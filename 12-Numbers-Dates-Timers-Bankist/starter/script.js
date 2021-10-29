5754b997138f2f3faae274c72372069dd0c57a3b
rhedwan <adesolaridwan2003@gmail.com>
2021-10-29 22:43:40 +0100

    Creating and working with dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -361,7 +361,7 @@ labelBalance.addEventListener('click', function(){
 
 });
  */
-
+/* 
 console.log(2 ** 53 -1);
 console.log(Number.MAX_SAFE_INTEGER);
 console.log(2 ** 53 + 1);
@@ -398,4 +398,39 @@ console.log(huge + ' is REALLY big !!!')
 
 // Divisions
 console.log(10n / 3n) ;
-console.log(10 / 3) ;
+console.log(10 / 3) ; */
+
+// Create a date
+/* 
+const now = new Date () ;
+console.log(now);
+
+console.log(new Date('Oct 29 2021 22:18:18'));
+console.log(new Date('May 05, 2003'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 34));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000)); */
+
+// Working with dates
+/* 
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142253380000));
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future); */
