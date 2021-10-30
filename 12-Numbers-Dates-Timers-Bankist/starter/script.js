edcd6f2dbfe7edac775c24958f3c8eaa1f523eef
rhedwan <adesolaridwan2003@gmail.com>
2021-10-30 21:13:32 +0100

    Operations with Dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2021-10-24T17:01:17.194Z',
+    '2021-10-29T10:36:17.929Z',
+    '2021-10-30T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -81,6 +81,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date){
+
+  const calcDaysPassed = (date1, date2) =>  Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24)) ;
+  const daysPassed = calcDaysPassed(new Date(), date) ;
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today' ;
+  if (daysPassed === 1) return 'Yesterday' ;
+  if (daysPassed <= 7) return `${daysPassed} days ago` ;
+  const day = `${date.getDate()}`.padStart(2,0) ;
+  const month = `${date.getMonth() + 1}`.padStart(2,0) ;
+  const year = date.getFullYear() ;
+  return  `${day}/${month}/${year}` ;
+  
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -88,12 +104,10 @@ const displayMovements = function (acc, sort = false) {
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2,0) ;
-    const month = `${date.getMonth() + 1}`.padStart(2,0);
-    const year = date.getFullYear() ;
-  
-    const displayDate = `${day}/${month}/${year}` ;
+    const displayDate = formatMovementDate(date) ;
+    
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -462,3 +476,12 @@ console.log(Date.now());
 
 future.setFullYear(2040);
 console.log(future); */
+
+/* 
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24) ;
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3 ,24)) ;
+console.log(days1); */
