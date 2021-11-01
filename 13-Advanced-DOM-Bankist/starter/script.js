4cedd7a438812124259fed7bf7a4bdacd08cd1b1
rhedwan <adesolaridwan2003@gmail.com>
2021-11-01 17:38:07 +0100

    Selecting, Creating and Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -31,3 +31,56 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header') ;
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections) ;
+
+// HTMLCollection
+document.getElementById('section--1')
+
+const allButtons = document.getElementsByTagName('button') ;
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+
+// Creating and inserting elements
+//.insertAdjacentHTML
+
+const message = document.createElement('div') ;
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.' ;
+message.innerHTML =  `We use cookies for improved functionality and
+ analytics.<button class="btn  btn--close-cookie"> Got it </button>` ;
+
+//  prepend add it as the first child of the target element
+// N:B Conventionally prepend and append can't be used together 
+// header.prepend(message) ;
+
+header.append(message) ;
+
+// this would make bothe the prepend and append appear
+// header.append(message.cloneNode(true)) ;
+
+// header.before(message) ;
+// header.after(message) ;
+
+// Delete Elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove() ;
+
+  // The previous way of doing it
+  // message.parentElement.removeChild(message) ;
+})
