1f72e1bd34b0f49402d40c999d12b3f4984489cf
rhedwan <adesolaridwan2003@gmail.com>
2021-11-02 16:25:03 +0100

    Implementing smooth scrolld

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -145,3 +145,32 @@ logo.classList.contains('c') ; // not includes
 // Don't use this. ut overwrite the existing values
 logo.className = 'ridwan';
  */
+
+// Implementing smooth scroll
+
+const btnScrollTo = document.querySelector('.btn--scroll-to') ;
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',function(e){
+  // const s1coords = section1.getBoundingClientRect() ;
+  // console.log(s1coords) ;
+
+  // console.log(e.target.getBoundingClientRect()) ;
+
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset) ;
+
+  // console.log('height/width viewport', document.documentElement.clientHeight,document.documentElement.clientWidth ) ;
+
+  // Scrolling
+  // console.log(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset) ;
+
+  // window.scrollTo({
+  //   left :s1coords.left + window.pageXOffset, 
+  //   top :s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // });
+
+  // Modern
+  section1.scrollIntoView({behavior: 'smooth'}) ;
+});
