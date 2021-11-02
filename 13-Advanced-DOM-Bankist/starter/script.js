e00cbbe17e63f0ecf7a1fd6c8b305b065abd909a
rhedwan <adesolaridwan2003@gmail.com>
2021-11-02 20:20:31 +0100

    Events Propagation in Practice

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -192,3 +192,31 @@ setTimeout(() => {
 //   alert('addEventListener: Great ! You are reading the heading:D')
 // };
 
+
+// rgb(255, 255, 2555)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min +1)+ min) ;
+const randomColor = () => `rgb(${randomInt(0, 256)},${randomInt(0, 256)}, ${randomInt(0, 256)})` ;
+
+document.querySelector('.nav__link').addEventListener('click', function(e){
+  e.preventDefault();
+  this.style.backgroundColor = randomColor() ;
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation() ;
+})
+
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  e.preventDefault();
+  this.style.backgroundColor = randomColor() ;
+  console.log('CONTAINER', e.target, e.currentTarget);
+
+})
+
+document.querySelector('.nav').addEventListener('click', function(e){
+  e.preventDefault();
+  this.style.backgroundColor = randomColor() ;
+  console.log('NAV', e.target, e.currentTarget);
+
+})
