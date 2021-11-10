a9f4d86141597898ca4c0e964dc8956eda5e37be
rhedwan <adesolaridwan2003@gmail.com>
2021-11-10 17:31:06 +0100

    Created the slider dots dynamically

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -246,9 +246,18 @@ imgTargets.forEach(img => imgObserver.observe(img)) ;
 const slides = document.querySelectorAll('.slide') ;
 const btnLeft = document.querySelector('.slider__btn--left') ;    
 const btnRight = document.querySelector('.slider__btn--right') ;    
+const dotContainer = document.querySelector('.dots') ;
+
 let currentSlide = 0 ;
 const maxSlide = slides.length ;
 
+const createDots = function(){
+  slides.forEach(function(_, i){
+    dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`) ; 
+  }) ;
+};
+
+createDots() ;
 // const slider  = document.querySelector('.slider') ;
 // slider.style.transform = 'scale(0.5)' ;
 // slider.style.overflow = 'visible' ;
