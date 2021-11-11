94e54845825b15ec1ee5d5d8603cfe1e79c61cfb
rhedwan <adesolaridwan2003@gmail.com>
2021-11-11 14:56:09 +0100

    Constructor function in OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// Constructor function
+const Person = function(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    // console.log(this);
+};  
+
+const john = new Person('John', 1990);       // new keyword is used to create an object
+console.log(john);
+
+// 1. New {} is created
+// 2. The constructor function is called, this = {}  
+// 3. {} is linked to a prototype
+// 4. function automatically return {}   
+
+const matilda = new Person('Matilda', 2017);  
+const jack = new Person('Jack', 1972);
+console.log(matilda, jack);
+
+console.log(john instanceof Person);
