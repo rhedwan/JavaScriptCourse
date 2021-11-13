a32ab7db189d6bd8fdacc42a7e783732dd4aac50
rhedwan <adesolaridwan2003@gmail.com>
2021-11-13 22:05:11 +0100

    Coding Challenge #2 OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -237,3 +237,39 @@ console.log(steven.__proto__ === PersonProto);
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
 sarah.calcAge();
+
+
+// Coding Challenge #2
+class CarCl {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed}km/h`);
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is decreasing to ${this.speed}km/h`);
+    }   
+
+    get speedUS() {
+        this.speed = this.speed / 1.6;
+        // return `The Current Speed is ${this.speed} in mi/h`
+        console.log(`The Current Speed is ${this.speed} in mi/h`);
+    }
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+        console.log(`The Current Speed is ${this.speed} in mi/h`);
+    }
+}
+
+
+const ford = new CarCl('Ford', 120);
+ford.accelerate();
+ford.brake();
+ford.speedUS;
+ford.speedUS = 100;
