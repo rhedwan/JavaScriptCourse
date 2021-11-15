363547130ede5162ee0e61c20e3e2a51f518b1a0
rhedwan <adesolaridwan2003@gmail.com>
2021-11-15 23:26:35 +0100

    Challenge #2 => Inheritance between classes:Car and EV

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -317,4 +317,51 @@ console.log(mike instanceof Object) ;
 
 // This makes the Student prototype itself rather than the Person prototype
 Student.prototype.constructor = Student;
-console.dir(Student.prototype.constructor)
+console.dir(Student.prototype.constructor);
+
+// Challenge #2
+const Car = function(make, speed) {
+    this.make = make ;
+    this.speed = speed ;
+};
+
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+}
+
+Car.prototype.brake = function() {
+    this.speed -= 5;
+    console.log(`${this.make} is decreasing to ${this.speed}km/h`);
+}   
+
+const EV = function(make, speed, charge) {
+    Car.call(this, make, speed)
+    this.charge = charge ;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function(chargeTo) {
+    this.charge = chargeTo;
+};
+
+// This would overwrite the exissting 'accelerate' in the inherited class
+EV.prototype.accelerate = function() {
+    this.speed += 20;
+    this.charge -= 1;
+    console.log(`${this.make} going at ${this.speed+2}km/h, with a charge of ${this.charge}%`)
+};
+
+EV.prototype.constructor = EV;
+
+const tesla = new EV('Tesla', 120, 23);
+console.log(tesla);
+
+tesla.chargeBattery(50);
+console.log(tesla);
+
+tesla.accelerate();
+tesla.brake();
+tesla.accelerate();
+tesla.accelerate();
