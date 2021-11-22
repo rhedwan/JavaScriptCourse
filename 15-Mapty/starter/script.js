6e6ff52fc0f415f4afc68acfab05b39cd53d9aa0
rhedwan <adesolaridwan2003@gmail.com>
2021-11-22 12:01:20 +0100

    Implemented Map EventListerner

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -20,6 +20,7 @@ if(navigator.geolocation)
         const coords = [latitude, longitude]
 
         const map = L.map('map').setView(coords, 14);
+        // console.log(map);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -28,6 +29,10 @@ if(navigator.geolocation)
         L.marker(coords).addTo(map)
             .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
             .openPopup();
+        // Map EventListerner
+        map.on('click', function(mapEvent){
+            console.log(mapEvent);
+        } );
     }, function () {
         alert('Geolocation is not supported by this browser.');
     } );
