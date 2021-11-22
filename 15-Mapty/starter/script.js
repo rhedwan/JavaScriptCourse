738bcace519b377d7d00b24815cf5f6f04187636
rhedwan <adesolaridwan2003@gmail.com>
2021-11-22 11:37:43 +0100

    Displaying Map using the Leaflet Library from docs

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,10 +13,19 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if(navigator.geolocation) 
     navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position);
         const { latitude, longitude } = position.coords;
         console.log(latitude, longitude);
         console.log(`https://www.google.com.ng/maps/@${latitude},${longitude}`)
+
+        const map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     }, function () {
         alert('Geolocation is not supported by this browser.');
     } );
