22a7547f4007e5c322f9256b2dbfb941f0d47a15
rhedwan <adesolaridwan2003@gmail.com>
2021-12-05 19:48:29 +0100

    Get neighbour country to make them inorder

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -56,7 +56,8 @@ const renderCountry = function (data) {
 };
 
 const getCountryAndNeighbour = function (country) {
-    
+
+    // AJAX call country 1
     const request = new XMLHttpRequest() ;
     request.open('GET', `https://restcountries.com/v2/name/${country}`);
     request.send();
@@ -65,7 +66,25 @@ const getCountryAndNeighbour = function (country) {
     request.addEventListener('load', function(){
         const [data] = JSON.parse(this.responseText);
         console.log(data);
+
+        // Rendercontry 1
         renderCountry(data);
+
+        // Get neighbour country (2)
+        const neighbour = data.borders[0];
+        if(!neighbour) return;
+
+        // AJAX call country 2
+
+        const request2 = new XMLHttpRequest() ;
+        request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+        request2.send();
+        request2.addEventListener('load', function(){
+            console.log(this.responseText)
+            const data2 = JSON.parse(this.responseText);
+            console.log(data2);
+            renderCountry(data2);
+        });
     });
 };
 
