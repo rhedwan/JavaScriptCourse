f8039556382935848e5a4630f0c97d8acbdabd80
rhedwan <adesolaridwan2003@gmail.com>
2021-12-05 18:15:42 +0100

    first AJAX cal; using XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,14 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest() ;
+request.open('GET', 'https://restcountries.com/v2/name/nigeria');
+request.send();
+console.log(request.responseText);
+
+request.addEventListener('load', function(){
+    console.log(this.responseText);
+    const data  = JSON.parse(this.responseText);
+    console.log(data);
+});
