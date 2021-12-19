01b9f79e9ad50b645151595ce9dc35a770e877f3
rhedwan <adesolaridwan2003@gmail.com>
2021-12-19 15:53:24 +0100

    Event loop in pratice part 2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -245,4 +245,8 @@ setTimeout(() => {
     console.log('0 sec tiner') ;
 }, 0);
 Promise.resolve('Resolved Promise 1').then(res => console.log(res)) ;
+Promise.resolve('Resolved Promise 2').then(res =>{ 
+    for (let i = 0; i < 10000 ; i++) {}
+    console.log(res)
+}) ;
 console.log('Test end') ;
