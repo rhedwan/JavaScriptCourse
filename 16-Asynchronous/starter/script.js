7028dc33c75eeac209a1953f78726defe586f388
rhedwan <adesolaridwan2003@gmail.com>
2021-12-22 10:44:31 +0100

    Coding Challenge #3 part 1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -505,3 +505,43 @@ Promise.any([
 .catch(err => console.error(err))
  */
 
+const imgContainer = document.querySelector('.images');
+
+const wait = function(seconds){
+    return new Promise(function(resolve){
+        setTimeout(resolve, seconds * 1000)
+    })
+};
+
+const createImage = function (imgPath) {
+    return new Promise(function(resolve, reject){
+        const img  = document.createElement('img') ;
+        img.src = imgPath ;
+        img.addEventListener('load', function () {
+            imgContainer.appendChild(img);    
+            resolve(img)
+        })
+        img.addEventListener('error', function () {
+            reject(new Error('Something Unexpected Occur'))
+        })
+    })
+};
+
+const loadNPause = async function(){
+    try{
+        // Load image 1
+        let img= await createImage(`img/img-1.jpg`) ;
+        console.log('Image 1 loaded') ;
+        await wait(2)
+        img.style.display ='none' ;
+
+        img = await createImage(`img/img-2.jpg`)
+        console.log('Image 2 loaded') ;
+        await wait(2)
+        img.style.display ='none' ;
+
+    }catch(err){
+        console.error(err) ;
+    }
+};
+loadNPause() ;
