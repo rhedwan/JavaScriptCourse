06ae0fff65aa9e4a768deb8984a4210f8ef25d35
rhedwan <adesolaridwan2003@gmail.com>
2021-12-23 08:06:48 +0100

    Importing of modules: exporting severals module

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,7 +1,8 @@
 // Importing module
-import {addToCart} from './shopppingCart.js';
+import {addToCart, totalPrice, totalQuantity} from './shopppingCart.js';
 
 console.log('Importing module') ;
 // console.log(shippingCost) ;
 
 addToCart('breads', 5)
+console.log(totalPrice, totalQuantity) ;
